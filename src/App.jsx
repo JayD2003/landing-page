3b50942b21e9be4b2c2bac07e67de291b672d3f7
JayD2003 <jayasaidodla@gmail.com>
2024-07-25 21:32:15 +0530

    added the initial files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
